87b476e6fc36bf022d098b9e0c7e12f6dd3d8968
a-stanisz <hello@adamstaniszewski.com>
2022-10-12 14:13:13 +0200

    Update files structure

--- services/order-service/index.ts
+++ services/order-service/index.ts
@@ -1,7 +1,17 @@
+import { logger } from 'logger'
+import { errorHandler, AppError } from 'error-handler'
 import { startWebServer } from './entry-points/api/server'
 
 async function start() {
   return Promise.all([startWebServer()])
 }
 
-export default start
+start()
+  .then((startResponses) => {
+    logger.info(`The app has started successfully ${startResponses}`)
+  })
+  .catch((error) => {
+    errorHandler.handleError(
+      new AppError('Startup failure', error.message, 500, false, error)
+    )
+  })
